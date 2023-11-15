5211a72967b3e0a2b7b3094f7999d99f46ba0d35
fall66-tech <141050847+fall66-tech@users.noreply.github.com>
2023-11-15 17:09:27 +0530

    inc/dec fuction

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [count , SetCount] = useState(0)
+
+  const inc = () => {
+    SetCount(count + 1);
+  }
+
+  const dec = () => {
+    SetCount(count - 1);
+  }
+
+
   return (
     <div className="App">
-      
+    <button onClick={dec}>-</button>
+      <span> {count} </span>
+      <button onClick={inc}>+</button>
+      <br />
+      <button onClick={()=> SetCount(0)}>reset</button>
     </div>
   );
 }
---
+++ src/components/UseSt.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function UseSt() {
+  return (
+    <div>
+      <div>
+      <button>-</button>
+      <span> 0 </span>
+      <button>+</button>
+    </div>
+    </div>
+  )
+}
+
+export default UseSt
